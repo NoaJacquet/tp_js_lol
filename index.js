665444487c7cf695f86f13d0edfe8347326d4968
NoaJacquet <noajacquet2004@gmail.com>
2024-03-27 16:37:02 +0100

    feat: ajout des notes par champion

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Note from './note.js';
+
 const listChampions = document.getElementById('liste-champions');
 const buttonVoirPrec = document.getElementById('voir-prec');
 const buttonVoirSuiv = document.getElementById('voir-suiv');
@@ -7,7 +9,6 @@ let idStartChamp = 0;
 buttonVoirPrec.disabled = true; 
 
 function get10Champions(indice){
-    console.log(indice)
     fetch('http://localhost:3000/champions', {
         method : 'GET'
     })
@@ -84,49 +85,75 @@ function getPrecChampions(){
         
         });
 };
-
-function getDetailChampion(id){
+async function getDetailChampion(id){
     fetch('http://localhost:3000/champions/'+id, {
         method : 'GET'
     })
     .then((response) => response.json())
-    .then((json) => {
+    .then(async (json) => {
         while (listChampions.firstChild) {
             listChampions.removeChild(listChampions.firstChild);
         }
-        
         for(const [key, value] of Object.entries(json)){
             const detail = document.createElement('p'); 
             detail.textContent = key + " : " + value;
             listChampions.appendChild(detail);
         }
+        const selectNote = document.createElement('select');
+        for (let i = 1; i <= 5; i++) {
+            const optionElement = document.createElement('option');
+            optionElement.value = i;
+            optionElement.textContent = i;
+            selectNote.appendChild(optionElement);
+        }
+        selectNote.id = "select-note";
+        listChampions.appendChild(selectNote);
+        const valid = document.createElement('button'); 
+        valid.textContent = "Ajoutez votre note";
+        valid.id = "valider-note";
+        valid.addEventListener('click', ajouterNote.bind(null, id));
+        listChampions.appendChild(valid);
         const detail = document.createElement('p'); 
-        console.log(getMoyenneNoteByChampion(id));
-        detail.textContent = "note : " +  getMoyenneNoteByChampion(id);
-        listChampions.appendChild(detail);
+        getNote(id).then(note => {
+            detail.textContent = "Note du champion : " + note;
         });
+        listChampions.appendChild(detail);
+    });
 };
 
-function getMoyenneNoteByChampion(idC){
+
+function ajouterNote(idC){
+    const note = new Note(document.getElementById("select-note").value, idC);
     fetch('http://localhost:3000/notes/', {
-        method : 'GET'
+        method : "POST",
+        body: JSON.stringify(note),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
     .then((response) => response.json())
     .then((json) => {
-        let somme = 0;
-        let cpt = 0;
-        for (let note of json){
-            if (note['idChampion'] === idC) {
-                somme += note['notation'];
-                cpt += 1;
-            }
-        }
-        console.log(somme /cpt);
-        return somme / cpt;
+        getDetailChampion(idC);
+    })
+    .catch((error) => {
     });
 }
+
+
+async function getNote(idC){
+    const response = await fetch('http://localhost:3000/notes/', {
+        method : "GET"
+    });
+    const json = await response.json();
+    for(let note of json){
+        if (note["idChampion"] === idC){
+            return note['notation'];
+        }
+    }
+    return 0; // Retourner une valeur par défaut si aucune note n'est trouvée
+}
+
 
 get10Champions(0);
 buttonVoirPrec.addEventListener('click', getPrecChampions);
 buttonVoirSuiv.addEventListener('click', getNextChampions);
-
--- note.js
+++ note.js
@@ -1,40 +1,43 @@
 export default class Note{
 
-    #id;
-    #notation;
-    #idChampion;
+    static lastId = 0;
 
-    constructor(id, notation, idChampion){
-        this.#id = id;
-        this.#notation = notation;
-        this.#idChampion = idChampion;
+    id;
+    notation;
+    idChampion;
+
+    constructor(notation, idChampion){
+        this.id = Note.lastId +1;
+        Note.lastId += 1;
+        this.notation = notation;
+        this.idChampion = idChampion;
     }
 
     get id(){
-        return this.#id;
+        return this.id;
     }
 
     get notation(){
-        return this.#notation;
+        return this.notation;
     }
 
     get idChampion(){
-        return this.#idChampion;
+        return this.idChampion;
     }
 
     set id(id){
-        this.#id = id;
+        this.id = id;
     }
 
     set notation(notation){
-        this.#notation = notation;
+        this.notation = notation;
     }
 
     set idChampion(idChampion){
-        this.#idChampion = idChampion;
+        this.idChampion = idChampion;
     }
 
     toString(){
-        return `id : ${this.#id}, notation : ${this.#notation}, idChampion : ${this.#idChampion}`;
+        return `id : ${this.id}, notation : ${this.notation}, idChampion : ${this.idChampion}`;
     }
 }
